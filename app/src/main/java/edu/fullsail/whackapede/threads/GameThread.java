9cd5c5a2b87d493b841b7c618385e8628e594d89
David Clark <daclark1@student.fullsail.edu>
2020-05-11 11:55:46 -0400

    After several fits at trying to use predefined vectors and drawables, resorted to drawing primitives in GameThread to build a proof-of-concept game arena, now composed of 4 major layers.  From bottom to top, they are Dirt/Earth, Below Ground Centipede Segments, Grass/Lawn with Holes, and Above Ground Centipede Segments.  Using Porter-Duff transfer method, clear holes can be painted or "cut into" the Grass/Lawn layer, exposing layers beneath it.  Painted in the aforementioned order, Game Thread now draws a static Game Arena to the Game Surface View of the Game Fragment, showing one centipede segment on top of the lawn, and one underground between the Dirt/Earth and Grass/Lawn layers, partially occluded.  The Dirt/Earth layer, of course, also shows through the holes that have been cut into the Grass/Lawn layer.

--- app/src/main/java/edu/fullsail/whackapede/threads/GameThread.java
+++ app/src/main/java/edu/fullsail/whackapede/threads/GameThread.java
@@ -8,9 +8,14 @@ MDV469-O, C202005-01
 package edu.fullsail.whackapede.threads;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
-import android.graphics.drawable.Drawable;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.view.SurfaceHolder;
+
+import androidx.core.content.ContextCompat;
 
 import edu.fullsail.whackapede.R;
 
@@ -30,10 +35,50 @@ public class GameThread extends Thread {
             Canvas canvas = surfaceHolder.lockCanvas();
 
             // TODO: Update game state and draw to canvas.
-            Drawable drawableEarth = context.getResources().getDrawable( R.drawable.game_layer_earth, null );
+            Paint paintBitmap = new Paint();
+            Paint paintHole = new Paint();
+            Paint paintAbove = new Paint();
+            Paint paintBelow = new Paint();
 
-            drawableEarth.setBounds( 0, 0, canvas.getWidth(), canvas.getHeight() );
-            drawableEarth.draw( canvas );
+            float radiusHole = (float) canvas.getWidth() / 7 / 2;
+            float radiusSegment = radiusHole * 0.75f;
+
+            int colorGrass = ContextCompat.getColor( context, R.color.grassGreen );
+            int colorEarth = ContextCompat.getColor( context, R.color.earthBrown );
+            int colorAbove = ContextCompat.getColor( context, R.color.dayBlue );
+            int colorBelow = ContextCompat.getColor( context, R.color.nightBlue );
+
+            Bitmap bitmapGrass = Bitmap.createBitmap( canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888 );
+            Canvas canvasGrass = new Canvas( bitmapGrass );
+
+            Bitmap bitmapAbove = Bitmap.createBitmap( canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888 );
+            Canvas canvasAbove = new Canvas( bitmapAbove );
+
+            Bitmap bitmapBelow = Bitmap.createBitmap( canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888 );
+            Canvas canvasBelow = new Canvas( bitmapBelow );
+
+            canvasGrass.drawColor( colorGrass );
+
+            paintHole.setAntiAlias( true );
+            paintHole.setXfermode( new PorterDuffXfermode( PorterDuff.Mode.CLEAR ) );
+
+            canvasGrass.drawCircle( (float) canvas.getWidth() / 7 + radiusHole, (float) canvas.getHeight() / 11 + radiusHole, radiusHole, paintHole );
+
+            paintAbove.setAntiAlias( true );
+            paintAbove.setColor( colorAbove );
+
+            canvasAbove.drawCircle( (float) canvas.getWidth() / 7 * 2 + radiusHole, (float) canvas.getHeight() / 11 * 2 + radiusHole, radiusSegment, paintAbove );
+
+            paintBelow.setAntiAlias( true );
+            paintBelow.setColor( colorBelow );
+
+            canvasBelow.drawCircle( (float) canvas.getWidth() / 7 * 1.25F + radiusHole, (float) canvas.getHeight() / 11 * 1.25F + radiusHole, radiusSegment, paintBelow );
+
+            canvas.drawColor( colorEarth );
+            canvas.drawBitmap( bitmapBelow, 0, 0, paintBitmap );
+            canvas.drawBitmap( bitmapGrass, 0, 0, paintBitmap );
+            canvas.drawBitmap( bitmapAbove, 0, 0, paintBitmap );
+
             surfaceHolder.unlockCanvasAndPost( canvas );
         }
     }
